d32dac97cc6d2c08c9efdfe7f78f42fd09e3d9ec
Lorenzo Ilardi <l.ilardi@reply.it>
2025-04-08 18:30:58 +0200

    Initial commit

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Lorenzo Ilardi on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
